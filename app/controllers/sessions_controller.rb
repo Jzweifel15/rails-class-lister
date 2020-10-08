91a73c4821aa7a84fdf7d45cc473cc5bce1ab787
Jzweifel15 <jzweifel15@yahoo.com>
2020-10-08 17:38:42 -0400

    Sign In and Sign Up Forms
    Created Sign in and Sign up forms for a student

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,18 @@
 class SessionsController < ApplicationController
+
+  def sign_in 
+  end
+
+  def create
+    @student = Student.find_by(school_email: params[:student][:school_email])
+    return head(:forbidden) unless @student && @student.authenticate(params[:student][:password])
+    session[:user_id] = @student.id 
+    redirect_to student_path(@student)
+  end
+
+  def destroy
+    session.clear 
+    redirect_to '/'
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   
-  root 'welcome#home'
+  root 'sessions#sign_in'
+  resources :sessions, only: [:sign_in, :create, :destroy]
 
   resources :students, only: [:index, :show, :new, :create]
 
