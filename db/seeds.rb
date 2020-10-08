24d90bd464f3935d32435b8ec17dc43716af7218
Jzweifel15 <jzweifel15@yahoo.com>
2020-10-08 05:37:58 -0400

    Seed Data
    seeded the database with dummy data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+50.times do 
+  name = Faker::Name.name
+  school_email = "#{name}@#{Faker::University.name}.com".delete " "
+  major = Faker::Educator.subject
+  Student.create(name: name, school_email: school_email, major: major)
+end
+
+25.times do 
+  name = Faker::Name.name 
+  school_email = "#{name}@#{Faker::University.name}.com".delete " "
+  department = Faker::Educator.subject 
+  Professor.create(name: name, school_email: school_email, department: department)
+end
+
+50.times do 
+  course_name = Faker::Educator.course_name
+  Class.create(course_name: course_name)
+end
