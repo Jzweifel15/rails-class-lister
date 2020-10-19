be85fe986d33ab62e0917ea709225c74cf3ee05d
Jzweifel15 <jzweifel15@yahoo.com>
2020-10-19 09:28:52 -0400

    OmniAuth Links
    third party authentication links added to the sign in page if a student would like to log in using either facebook or github

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   
   root 'sessions#sign_in'
   resources :sessions, only: [:sign_in, :create, :destroy]
+  get '/auth/facebook/callback', to: 'sessions#create'
+  get '/auth/github/callback', to: 'sessions#create'
 
   resources :students, only: [:index, :new, :create]
 
